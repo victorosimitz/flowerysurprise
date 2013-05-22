95a13e53d7a97a71ceb90adf9c367fbab576060a
Victor Osimitz <v.osimitz@gmail.com>
2013-05-22 14:04:31 -0700

    some cleanup, bare-bones home page with fb login

--- flowerysurprise.js
+++ flowerysurprise.js
@@ -1,16 +1,5 @@
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to flowerysurprise.";
-  };
-
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      alert('clicked');
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
+  Template.hello.events({   });
 }
 
 if (Meteor.isServer) {
